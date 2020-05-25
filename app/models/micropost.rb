c70bf2955129c944c26bca90778ad776c12c0983
kokoro.muneguchi <gxswf_space69@yahoo.co.jp>
2020-05-25 12:11:18 +0000

    saiteisyutu

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -2,7 +2,7 @@ class Micropost < ApplicationRecord
   belongs_to :user
 
   has_many :likes
-  has_many :liked_user, through: :likes, source: :micropost
+  has_many :liked_user, dependent: :destroy, through: :likes, source: :micropost
 
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 255 }
